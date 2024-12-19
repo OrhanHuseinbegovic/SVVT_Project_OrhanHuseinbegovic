f5a3460022155ab469e909bf9114a30159dd0896
OrhanHuseinbegovic <o.huseinbegovic@gmail.com>
2024-12-19 22:58:31 +0100

    Added test cases for test scenario Login

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SVVT_Project_OrhanHuseinbegovic
+
+Hello! 
+
+This is a project made in Java, for testing out parts of the 
+web platform / application for booking apartments (and publishing them).
+
+This project was entirely made by 2 International Burch University students: 
+1. Sadžida Rakovac
+2. Orhan Huseinbegović
+
+Who are at the moment last year student of Information technologies.
+If you want to know more about this project, feel free to contact the owner of this reposiroty.
---
+++ src/test/java/LoginForm.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoginForm {
+
+    private static WebDriver webDriver;
+    private static String baseUrl;
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/Users/orhanhuseinbegovic/Downloads/chromedriver-mac-arm64/chromedriver"); // specify the path to chromedriver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        webDriver = new ChromeDriver(options);
+        baseUrl = "https://www.korisnici.apartmanija.hr/login";
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        // Close the browser
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+
+    //Login scenario testing
+    @Test
+    void testEmptyLoginForm() {
+        webDriver.get(baseUrl);
+        WebElement submitButton = webDriver.findElement(By.cssSelector("button[type='submit']"));
+
+        submitButton.click();
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@id='loginForm']/div/ul/li")));
+
+        assertEquals("Neispravna e-mail adresa!", errorMessage.getText(), "Error message did not match the expected text.");
+    }
+
+    @Test
+    public void testLoginTitle() throws InterruptedException {
+        webDriver.get(baseUrl);
+        String actualTitle = webDriver.getTitle();
+        System.out.println("Actual title: " + actualTitle);
+        assertEquals("Korisničko sučelje - Apartmanija.hr", actualTitle, "Title does not match");
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void testInputWrongCredentials() {
+        webDriver.get(baseUrl);
+        webDriver.manage().window().setSize(new Dimension(1470, 822));
+
+        webDriver.findElement(By.name("LoginForm[username]")).sendKeys("someemail@gmail.com");
+        webDriver.findElement(By.name("LoginForm[password]")).sendKeys("somepassword");
+
+        webDriver.findElement(By.cssSelector("button")).click();
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li")));
+
+        assertEquals("Neispravni korisnički podaci!", errorMessage.getText());
+    }
+
+
+
+}
