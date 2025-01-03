f411919e5a084ad48d3c8371b4efb4d033f70f14
OrhanHuseinbegovic <o.huseinbegovic@gmail.com>
2025-01-03 13:09:27 +0100

    Search FIlter Testing

--- src/test/java/SearchFilterTesting.java
+++ src/test/java/SearchFilterTesting.java
@@ -79,6 +79,39 @@ public class SearchFilterTesting {
         assertTrue(apartmentName);
     }
 
+    @Test
+    void testReviewed() throws InterruptedException {
+        webDriver.get(baseUrl + "/pretraga/apartmani/r:split-i-okolica+c:gradac+osobe:1");
+        Thread.sleep(1500);
+        WebElement sort = webDriver.findElement(By.xpath("/html/body/div[9]/div[4]/button"));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", sort);
+        Thread.sleep(1500);
+        webDriver.findElement(By.xpath("/html/body/div[9]/div[4]/button")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.xpath("/html/body/div[11]/div/div[2]/div[2]/button")).click();
+        Thread.sleep(1000);
+        webDriver.findElement(By.linkText("Broju ocjena")).click();
+
+        String firstApartment = webDriver.findElement(By.xpath("//*[@id=\"ads_items\"]/div[1]/a/div[2]/span")).getText();
+
+        WebElement lastAp = webDriver.findElement(By.xpath("//*[@id=\"ads_items\"]/div[21]/a/div[2]/span"));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", lastAp);
+        Thread.sleep(1500);
+
+        String lastApartment = webDriver.findElement(By.xpath("//*[@id=\"ads_items\"]/div[21]/a/div[2]/span")).getText();
+
+        if (firstApartment.contains("Apartmani Lujanka")) {
+            firstApartment = "Apartmani Lujanka";
+        }
+        if (lastApartment.contains("Apartmani Villa Ana")) {
+            lastApartment = "Apartmani Villa Ana";
+        }
+
+        assertNotEquals(firstApartment, lastApartment, "There is no more than 1 apartment here");
+        assertEquals("Apartmani Lujanka", firstApartment);
+        assertEquals("Apartmani Villa Ana", lastApartment);
+    }
+
 
 
 }
