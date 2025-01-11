f56e543a3aa71651cd52b6a91726439b43d55d98
OrhanHuseinbegovic <o.huseinbegovic@gmail.com>
2025-01-11 19:40:43 +0100

    Added 2 more tests

--- src/test/java/SocialMediaTesting.java
+++ src/test/java/SocialMediaTesting.java
@@ -38,10 +38,8 @@ public class SocialMediaTesting {
 
     @Test
     void testSocialMediaRedirecting() throws InterruptedException {
-        // Open the base URL
         webDriver.get(baseUrl);
 
-        // Define the xpaths for social media icons and their expected URLs
         String[][] socialMediaData = {
                 {"/html/body/div[20]/div/div[2]/div[1]/a[1]", "https://www.facebook.com/apartmanija"},
                 {"/html/body/div[20]/div/div[2]/div[1]/a[3]", "https://x.com/apartmanija?mx=2"},
@@ -57,15 +55,12 @@ public class SocialMediaTesting {
             String iconXPath = data[0];
             String expectedUrl = data[1];
 
-            // Find the social media icon
-            WebElement socialMediaIcon = new WebDriverWait(webDriver, Duration.ofSeconds(10))
+             WebElement socialMediaIcon = new WebDriverWait(webDriver, Duration.ofSeconds(10))
                     .until(ExpectedConditions.elementToBeClickable(By.xpath(iconXPath)));
 
             Thread.sleep(1000);
-            // Click the icon (opens a new tab or window)
             socialMediaIcon.click();
 
-            // Wait for the new tab/window and switch to it
             new WebDriverWait(webDriver, Duration.ofSeconds(10)).until(driver -> webDriver.getWindowHandles().size() > 1);
 
             String originalWindow = webDriver.getWindowHandle();
@@ -76,17 +71,43 @@ public class SocialMediaTesting {
                 }
             }
 
-            // Verify the URL of the new tab
             String currentUrl = webDriver.getCurrentUrl();
             Thread.sleep(1000);
             assertTrue(currentUrl.contains(expectedUrl), "Expected URL to contain: " + expectedUrl + ", but got: " + currentUrl);
             Thread.sleep(1000);
 
-            // Close the new tab and switch back to the original window
             webDriver.close();
             webDriver.switchTo().window(originalWindow);
         }
     }
 
+    @Test
+    void testSocialMediaOnApartment() throws InterruptedException {
+        webDriver.get(baseUrl);
+
+        Thread.sleep(500);
+        WebElement cookiesSubmit = webDriver.findElement(By.cssSelector(".btn_ok"));
+        Thread.sleep(1000);
+
+        cookiesSubmit.click();
+
+        Thread.sleep(500);
+        WebElement firstApartment = webDriver.findElement(By.xpath("/html/body/div[18]/div/div[1]"));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(true);", firstApartment);
+        Thread.sleep(500);
+
+        firstApartment.click();
+        Thread.sleep(500);
+
+        WebElement podijeli = webDriver.findElement(By.linkText("Podijeli"));
+        podijeli.click();
+        Thread.sleep(500);
+
+        webDriver.findElement(By.xpath("//*[@id=\"mod-social-share\"]/div/div/div[2]/a[9]/i")).click();
+        Thread.sleep(500);
+        String link = webDriver.findElement(By.xpath("//*[@id=\"mod-copy-link\"]/div/div/div[2]/input")).getAttribute("value");
+
+        assertEquals(webDriver.getCurrentUrl(), link);
+    }
 
 }
